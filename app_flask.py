e896b21c0db1ebd00ef17c952ebc22b94e110497
Serhiy <sergiyclas@gmail.com>
2024-12-14 01:55:49 +0200

    Try to use multiprocessing

--- app_flask.py
+++ app_flask.py
@@ -1,6 +1,6 @@
-import threading
 import asyncio
 import logging
+from multiprocessing import Process
 from aiogram import Bot, Dispatcher
 from telethon import TelegramClient
 from flask import Flask, jsonify, request
@@ -31,7 +31,6 @@ logging.basicConfig(level=logging.INFO)
 # Scheduler for periodic tasks
 scheduler = BackgroundScheduler()
 
-
 @app.route('/')
 def index():
     """Home page to show the status of the bot."""
@@ -41,13 +40,9 @@ def index():
 
 @app.route('/echo', methods=['GET'])
 def echo():
-    # Get all query parameters from the request
+    """Echo query parameters."""
     query_params = request.args
-
-    # Return the parameters back as a JSON response
-    return jsonify({
-        "echoed_data": query_params
-    })
+    return jsonify({"echoed_data": query_params})
 
 
 async def start_bot():
@@ -58,14 +53,13 @@ async def start_bot():
     await dp.start_polling(bot, skip_updates=True, close_loop=False)
 
 
-def bot_thread(loop):
-    """Function to run bot in the provided asyncio event loop."""
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(start_bot())
+def start_bot_process():
+    """Run the bot in a separate process."""
+    asyncio.run(start_bot())
 
 
 def start_scheduler():
-    """Start the scheduler."""
+    """Run the scheduler."""
     scheduler.add_job(
         lambda: asyncio.run(check_and_notify_groups_for_birthday(bot)),
         'cron',
@@ -76,16 +70,23 @@ def start_scheduler():
     scheduler.start()
 
 
+def start_flask():
+    """Run the Flask app."""
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
 if __name__ == '__main__':
-    # Create a new asyncio event loop for the bot
-    bot_loop = asyncio.new_event_loop()
-
-    # Start the bot in a separate thread with the new event loop
-    bot_thread_instance = threading.Thread(target=bot_thread, args=(bot_loop,), daemon=True)
-    bot_thread_instance.start()
-
-    # Start the scheduler
+    # Start scheduler
     start_scheduler()
 
-    # Run the Flask app
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    # Create separate processes for the bot and Flask app
+    bot_process = Process(target=start_bot_process)
+    flask_process = Process(target=start_flask)
+
+    # Start both processes
+    bot_process.start()
+    flask_process.start()
+
+    # Wait for both processes to finish
+    bot_process.join()
+    flask_process.join()
